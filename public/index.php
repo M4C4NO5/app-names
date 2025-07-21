70aea9cd3b132842923dc6e23d632dcafbf26240
M4C4NO5 <139782321+M4C4NO5@users.noreply.github.com>
2025-07-20 20:31:34 -0500

    Add: Index

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    <meta charset="UTF-8">
+    <title>Añadir nombre</title>
+    </head>
+    <body>
+        <h1>Formulario para ingresar nombre</h1>
+        <form action="save.php" method="POST">
+            <input type="text" id="nombre" name="nombre" required placeholder="Ingrese un nombre">
+            <button type="submit">Enviar</button>
+    </body>
+</html>
